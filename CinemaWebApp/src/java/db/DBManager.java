87e0c454d85a162d89948a5548ccead8c696c587
b0sk <b0sk@users.noreply.github.com>
2015-06-27 18:08:35 +0200

    Add DBManager and a function to get all the films

---
+++ CinemaWebApp/src/java/db/DBManager.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Patrik
+ */
+public class DBManager implements Serializable {
+    private transient Connection con;
+    
+    public DBManager(String dburl) throws SQLException {
+
+        try {
+
+            Class.forName("org.apache.derby.jdbc.ClientDriver", true, getClass().getClassLoader());
+
+        } catch(Exception e) {
+            throw new RuntimeException(e.toString(), e);
+        }
+        
+        Connection con = DriverManager.getConnection(dburl);
+        this.con = con;
+
+    }
+    
+    public static void shutdown() {
+        try {
+            DriverManager.getConnection("jdbc:derby:;shutdown=true");
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).info(ex.getMessage());
+        }
+    }
+    
+    /**
+     * Ottiene la lista ti tutti i film dal DB
+     * @return ArrayList contente tutti i film
+     * @throws SQLException 
+     */
+    public List<Film> getFilms() throws SQLException {
+        List<Film> films = new ArrayList<Film>();
+        
+        PreparedStatement stm = con.prepareStatement("SELECT * FROM Film");
+        try{
+            ResultSet rs = stm.executeQuery();
+            try{
+                while(rs.next()){
+                    Film f = new Film();
+                    f.setTitolo(rs.getString("TITOLO"));
+                    f.setTrama(rs.getString("TRAMA"));
+                    f.setDurata(rs.getInt("DURATA"));
+                    f.setUriLocandina(rs.getString("URI_LOCANDINA"));
+                    f.setUrlTrailer(rs.getString("RUL_TRAILER"));
+                    
+                    films.add(f);
+                }
+            } finally {
+                rs.close();
+            }
+        } finally {
+            stm.close();
+        }
+        
+        return films;
+    }
+    
+    
+}
