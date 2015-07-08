57cab46da9282ffa04de088c7f733437a1addcee
b0sk <b0sk@users.noreply.github.com>
2015-07-08 18:40:44 +0200

    Add working RegistrazioneServlet

---
+++ CinemaWebApp/src/java/servlet/RegistrazioneServlet.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import db.DBManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Patrik
+ */
+@WebServlet(name = "RegistrazioneServlet", urlPatterns = {"/RegistrazioneServlet"})
+public class RegistrazioneServlet extends HttpServlet {
+
+    private DBManager manager;
+
+    @Override
+    public void init() throws ServletException {
+        // inizializza il DBManager dagli attributi di Application
+        this.manager = (DBManager) super.getServletContext().getAttribute("dbmanager");
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            if (request.getParameter("inputEmail") == null || request.getParameter("inputPassword") == null || request.getParameter("repeatPassword") == null) {
+                request.setAttribute("errorCode", 1);
+                request.getRequestDispatcher("registrazione.jsp").forward(request, response);
+            } else {
+                String email = request.getParameterValues("inputEmail")[0];
+                String password1 = request.getParameterValues("inputPassword")[0];
+                String password2 = request.getParameterValues("repeatPassword")[0];
+
+                if (!password1.equals(password2)) {
+                    //errore
+                    request.setAttribute("errorCode", 2);
+                    request.getRequestDispatcher("registrazione.jsp").forward(request, response);
+                } else {
+                    // prova a registrare l'utente nel DB
+                    boolean success = manager.registraUtente(email, password1);
+                    if (success == true) {
+                        request.setAttribute("errorCode", 0);
+                        request.getRequestDispatcher("login.jsp").forward(request, response);
+
+                    } else {
+                        request.setAttribute("errorCode", 3);
+                        request.getRequestDispatcher("registrazione.jsp").forward(request, response);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrazioneServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrazioneServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
