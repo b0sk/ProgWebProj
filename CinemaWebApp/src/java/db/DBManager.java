d6f81527a3b38ea9f2650b7dbbe66c5221087951
b0sk <b0sk@users.noreply.github.com>
2015-07-20 16:05:04 +0200

    Add GetFilmById(..) and GetSpettacoli(..) in DBManager.java

--- CinemaWebApp/src/java/db/DBManager.java
+++ CinemaWebApp/src/java/db/DBManager.java
@@ -20,23 +20,24 @@ import java.util.logging.Logger;
  * @author Patrik
  */
 public class DBManager implements Serializable {
+
     private transient Connection con;
-    
+
     public DBManager(String dburl) throws SQLException {
 
         try {
 
             Class.forName("org.apache.derby.jdbc.ClientDriver", true, getClass().getClassLoader());
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e.toString(), e);
         }
-        
+
         Connection con = DriverManager.getConnection(dburl);
         this.con = con;
 
     }
-    
+
     public static void shutdown() {
         try {
             DriverManager.getConnection("jdbc:derby:;shutdown=true");
@@ -44,28 +45,28 @@ public class DBManager implements Serializable {
             Logger.getLogger(DBManager.class.getName()).info(ex.getMessage());
         }
     }
-    
-    
+
     /**
      * Ottiene la lista ti tutti i film dal DB
+     *
      * @return ArrayList contente tutti i film
-     * @throws SQLException 
+     * @throws SQLException
      */
     public List<Film> getFilms() throws SQLException {
         List<Film> films = new ArrayList<Film>();
-        
+
         PreparedStatement stm = con.prepareStatement("SELECT * FROM Film");
-        try{
+        try {
             ResultSet rs = stm.executeQuery();
-            try{
-                while(rs.next()){
+            try {
+                while (rs.next()) {
                     Film f = new Film();
                     f.setTitolo(rs.getString("TITOLO"));
                     f.setTrama(rs.getString("TRAMA"));
                     f.setDurata(rs.getInt("DURATA"));
                     f.setUriLocandina(rs.getString("URI_LOCANDINA"));
                     f.setUrlTrailer(rs.getString("URL_TRAILER"));
-                    
+
                     films.add(f);
                 }
             } finally {
@@ -74,16 +75,75 @@ public class DBManager implements Serializable {
         } finally {
             stm.close();
         }
-        
+
         return films;
     }
+
+    /**
+     * Ottiene un Film dal DB in base al suo ID
+     *
+     * @param idFilm ID del film da ottenere
+     * @return Film desiderato
+     * @throws SQLException
+     */
+    public Film getFilmById(String idFilm) throws SQLException {
+        Film film = new Film();
+
+        PreparedStatement stm = con.prepareStatement("SELECT * FROM Film WHERE ID_FILM = ?");
+        try {
+            stm.setString(1, idFilm);
+            ResultSet rs = stm.executeQuery();
+            try {
+                rs.next();
+                film.setTitolo(rs.getString("TITOLO"));
+                film.setTrama(rs.getString("TRAMA"));
+                film.setDurata(rs.getInt("DURATA"));
+                film.setUriLocandina(rs.getString("URI_LOCANDINA"));
+                film.setUrlTrailer(rs.getString("URL_TRAILER"));
+            } finally {
+                rs.close();
+            }
+        } finally {
+            stm.close();
+        }
+
+        return film;
+    }
     
+    public List<Spettacolo> getSpettacoli(String idFilm) throws SQLException {
+        List<Spettacolo> spettacoli = new ArrayList<Spettacolo>();
+
+        PreparedStatement stm = con.prepareStatement("SELECT * FROM Spettacolo WHERE ID_FILM = ?");
+        try {
+            stm.setString(1, idFilm);
+            ResultSet rs = stm.executeQuery();
+            try {
+                while (rs.next()) {
+                    Spettacolo s = new Spettacolo();
+                    s.setIdSala(rs.getInt("ID_SALA"));
+                    s.setDataOra(rs.getTimestamp("DATA_ORA"));
+                    s.setIdFilm(Integer.parseInt(idFilm));
+                    s.setIdSpettacolo(rs.getInt("ID_SPETTACOLO"));
+
+                    spettacoli.add(s);
+                }
+            } finally {
+                rs.close();
+            }
+        } finally {
+            stm.close();
+        }
+
+        return spettacoli;
+    }
+
     /**
      * Registra un nuovo utente
+     *
      * @param email l'indirizzo email dell'utente
      * @param password la password dell'utente
      * @return true se l'utente viene registrato correttamente, false altrimenti
-     * @throws SQLException 
+     * @throws SQLException
      */
     public boolean registraUtente(String email, String password) throws SQLException {
         // Controlla se l'utente è gia registrato con la stessa email
@@ -93,13 +153,13 @@ public class DBManager implements Serializable {
         try {
             stm.setString(1, email);
             ResultSet rs = stm.executeQuery();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 cnt = rs.getInt(1);
             }
-            
+
             // Se trova un utente con la stessa mail non esegue l'insert
-            if(cnt != 0){
+            if (cnt != 0) {
                 retval = false;
             } else {
                 // Se non c'è gia lo iserisce
@@ -112,9 +172,9 @@ public class DBManager implements Serializable {
         } finally {
             stm.close();
         }
-        
+
         return retval;
-        
+
     }
-    
+
 }
