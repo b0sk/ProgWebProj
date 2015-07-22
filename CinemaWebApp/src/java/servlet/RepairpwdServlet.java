39dfc03c7644688208fccf8f3c9e66cc3f105cd9
b0sk <b0sk@users.noreply.github.com>
2015-07-22 13:57:02 +0200

    Rimossa parte inutile in RepairpwdServlet

--- CinemaWebApp/src/java/servlet/RepairpwdServlet.java
+++ CinemaWebApp/src/java/servlet/RepairpwdServlet.java
@@ -77,19 +77,6 @@ public class RepairpwdServlet extends HttpServlet {
             rd.forward(request, response);
         }
 
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet repairpwdServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet repairpwdServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     private void sendMail(String toEmail, String psw) {
